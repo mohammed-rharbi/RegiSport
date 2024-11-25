fde75f4e78ea75bad56f14b87f8c17f5544cc117
mohammed-rharbi <rharbi383@gmail.com>
2024-11-25 14:38:54 +0100

    feat : add users  files service controller entity and dto

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [ ConfigModule.forRoot({isGlobal: true}) ,
              MongooseModule.forRoot(process.env.DB),
+             UsersModule,
              
    ],
   controllers: [AppController],
