2affaa5bdedee85e9a5cf8587d88be08479bb37d
mohammed-rharbi <rharbi383@gmail.com>
2024-11-25 14:39:26 +0100

    feat : add users  files

---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema ,SchemaFactory } from "@nestjs/mongoose";
+import { Document , Types } from "mongoose";
+
+
+
+@Schema()
+export class User extends Document {
+
+
+    @Prop({required:true})
+    userName: string ;
+
+    @Prop({required:true})
+    email: string ;
+
+    @Prop({required: true})
+    password: string ;
+
+    @Prop({enum:['participant' , 'user'] , default:'user'})
+    role: string ;
+
+    @Prop({min:10 , max:15})
+    phone: number ;
+
+
+
+}
