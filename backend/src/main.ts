1b535e6c38c51585dd826243c2073ebc2c95c22f
mohammed-rharbi <rharbi383@gmail.com>
2024-11-25 12:02:42 +0100

    feat: connect mongodb

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,11 +6,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'], 
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
 
   await app.listen(process.env.PORT ?? 3000);
+  Logger.log(`this app is running at : http://localhost/${process.env.PORT ?? 3000}`,'Bootsrap')
 }
 bootstrap();
