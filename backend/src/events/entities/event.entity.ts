3c81da9488946dd5e758d26dc6ba5e02694a4637
mohammed-rharbi <rharbi383@gmail.com>
2024-11-26 14:44:46 +0100

    refactor: modify Event entity structure

--- backend/src/events/entities/event.entity.ts
+++ backend/src/events/entities/event.entity.ts
@@ -1,1 +1,28 @@
-export class Event {}
+import { Document } from "mongoose";
+import { Prop , Schema , SchemaFactory } from "@nestjs/mongoose";
+
+
+
+
+export class Event extends Document {
+
+
+    @Prop({required:true})
+    title: string ;
+
+    @Prop({required: true})
+    image: string ;
+
+    @Prop({required: true})
+    desc: string ;
+
+
+
+  
+
+
+
+
+
+}
+
