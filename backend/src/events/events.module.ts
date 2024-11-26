28e9a6065959ae95441dcfb52a065436c3413106
mohammed-rharbi <rharbi383@gmail.com>
2024-11-26 23:38:03 +0100

    chore: register new Event service and repository in module

--- backend/src/events/events.module.ts
+++ backend/src/events/events.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
+import { EventRepositorie } from './events.repositorie';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Event, EventShema } from './entities/event.entity';
 
 @Module({
+
+  imports: [ MongooseModule.forFeature([{ name: Event.name , schema: EventShema }])] ,
   controllers: [EventsController],
-  providers: [EventsService],
+  providers: [EventsService , EventRepositorie],
 })
 export class EventsModule {}
