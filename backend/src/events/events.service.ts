0b8d753c1636a609f957f5dce803d10a5e12e0c8
mohammed-rharbi <rharbi383@gmail.com>
2024-11-26 23:38:12 +0100

    feat: implement business logic for event management

--- backend/src/events/events.service.ts
+++ backend/src/events/events.service.ts
@@ -1,26 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { EventRepositorie } from './events.repositorie';
+import { Event } from './entities/event.entity';
 
 @Injectable()
 export class EventsService {
-  create(createEventDto: CreateEventDto) {
-    return 'This action adds a new event';
+
+  constructor( private readonly eventRepository: EventRepositorie ){}
+
+
+
+  async  create(eventDataDto: CreateEventDto): Promise<Event> {
+
+    try{
+
+
+      return await this.eventRepository.create(eventDataDto)
+
+    }catch(err: any){
+
+      throw new BadRequestException('error while creating the event' + err)
+
+    }
   }
 
   findAll() {
     return `This action returns all events`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} event`;
   }
 
-  update(id: number, updateEventDto: UpdateEventDto) {
+  update(id: string, updateEventDto: UpdateEventDto) {
     return `This action updates a #${id} event`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} event`;
   }
 }
