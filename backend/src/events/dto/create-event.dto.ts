8e964960fe89e988d20c32228f22e00a2cfa8450
mohammed-rharbi <rharbi383@gmail.com>
2024-11-26 23:37:49 +0100

    feat: enhance event creation DTO with new properties

--- backend/src/events/dto/create-event.dto.ts
+++ backend/src/events/dto/create-event.dto.ts
@@ -1,8 +1,25 @@
-import { IsNotEmpty , IsString , IsDate  } from "class-validator";
+import { IsNotEmpty , IsString , IsDate, IsAlpha   } from "class-validator";
 
 
 
 export class CreateEventDto {
+
+@IsString()
+@IsNotEmpty()
+@IsAlpha()
+title: string ;
+
+@IsDate()
+@IsNotEmpty()
+date: Date ;
+
+@IsString()
+@IsNotEmpty()
+description: string;
+
+@IsString()
+@IsNotEmpty()
+location: string
 
 
 
