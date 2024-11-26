2efdcd69eb244b98e5d17cbf14fe09103455ad3e
mohammed-rharbi <rharbi383@gmail.com>
2024-11-26 23:37:58 +0100

    feat: update Event controller with new endpoints

--- backend/src/events/events.controller.ts
+++ backend/src/events/events.controller.ts
@@ -1,16 +1,33 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, BadRequestException, UseInterceptors } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { Event } from './entities/event.entity';
 
 @Controller('events')
 export class EventsController {
+
   constructor(private readonly eventsService: EventsService) {}
 
-  @Post()
-  create(@Body() createEventDto: CreateEventDto) {
-    return this.eventsService.create(createEventDto);
+
+
+  @Post('create/event')
+  async create(@Body() createEventDto: CreateEventDto): Promise<{message: string}> {
+
+    try{
+
+    const event = await this.eventsService.create(createEventDto);
+
+    return {message: 'event created successfully'}
+
+    }catch(err: any){
+
+      throw new BadRequestException('error for creating event' + err);
+    }
+
+
   }
+
 
   @Get()
   findAll() {
@@ -19,16 +36,16 @@ export class EventsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.eventsService.findOne(+id);
+    return this.eventsService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
-    return this.eventsService.update(+id, updateEventDto);
+    return this.eventsService.update(id, updateEventDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.eventsService.remove(+id);
+    return this.eventsService.remove(id);
   }
 }
