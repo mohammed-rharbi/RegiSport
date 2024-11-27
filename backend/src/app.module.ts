3cd209c2fab4aea267ad0e6d3ab2fb1283dc061f
mohammed-rharbi <rharbi383@gmail.com>
2024-11-27 12:10:15 +0100

    chore: update AppModule to reflect new event features

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,12 +5,16 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { EventsModule } from './events/events.module';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
   imports: [ ConfigModule.forRoot({isGlobal: true}) ,
              MongooseModule.forRoot(process.env.DB),
              UsersModule,
              EventsModule,
+             MulterModule.register({
+              dest: './uploads'
+             }),
              
    ],
   controllers: [AppController],
