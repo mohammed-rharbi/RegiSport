4937c7cb04e292e6f19bb66859c3816e105eff66
mohammed-rharbi <rharbi383@gmail.com>
2024-11-27 12:10:39 +0100

    feat: enhance Event repository with additional methods

--- backend/src/events/events.repositorie.ts
+++ backend/src/events/events.repositorie.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Event } from "./entities/event.entity";
 import { Model } from "mongoose";
@@ -13,7 +13,7 @@ export class EventRepositorie {
     constructor(@InjectModel(Event.name) private readonly eventModel : Model<Event> ){}
 
 
-        async create(eventData : any ): Promise<Event>{
+        async create(eventData :  CreateEventDto ): Promise<Event>{
 
         const event = new this.eventModel(eventData);
 
@@ -26,14 +26,29 @@ export class EventRepositorie {
             return await this.eventModel.findById(id).exec();
         }
 
-        async update(id: string , eventData : UpdateEventDto): Promise<Event>{
+        async getAllEvents(): Promise<Event[]>{
 
+
+            return await this.eventModel.find().exec();
+
+        }
+
+        async update(id: string , eventData : UpdateEventDto): Promise<Event | null>{
+           console.log(eventData);
+           
             return await this.eventModel.findByIdAndUpdate(id , eventData , {new: true}).exec();
         }
 
-        async delete(id: string ): Promise<Event>{
 
-            return await this.eventModel.findByIdAndDelete(id).exec();;
+        async delete(id: string ): Promise<{message: string}>{
+
+            const event =  await this.eventModel.findByIdAndDelete(id).exec();
+
+            if(!event){
+                throw new BadRequestException('event does not exist');
+            }
+
+            return {message: 'event deletd successfully'};
         }
 
 
