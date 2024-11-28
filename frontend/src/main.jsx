107a1c9a808f443a7e49104522e8f1161d27917b
mohammed-rharbi <rharbi383@gmail.com>
2024-11-28 23:24:01 +0100

    fix: resolve routing issue in main component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './hooks/authContext.jsx'
+import { EventProvider } from './hooks/eventsContext.jsx'
 createRoot(document.getElementById('root')).render(
+
   <AuthProvider>
+    <EventProvider>
     <App />
+    </EventProvider>,
   </AuthProvider>,
 )
