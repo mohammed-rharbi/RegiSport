3e01026290ba400b30272e92b5f4f4a7e4a47564
mohammed-rharbi <rharbi383@gmail.com>
2024-11-28 23:24:13 +0100

    feat: enhance dashboard functionality

--- frontend/src/pages/dashBoard.jsx
+++ frontend/src/pages/dashBoard.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
+import DashCard from '../components/ui/dashCard'
 
 export default function DashBoard() {
   return (
+
+
     <div className="flex h-screen">
 
-      <div className="flex-1 bg-gray-100 p-6">
+      <div className="flex-1 rounded-lg bg-gray-800 p-6">
 
-        <div className="flex justify-between items-center mb-6">
-          <h1 className="text-3xl font-semibold">Dashboard Overview</h1>
-        </div>
+
+<div className='flex justify-center gap-4 mb-6'>
+<DashCard/>
+<DashCard/>
+<DashCard/>
+</div>
 
 
         <div>
+
           <h2 className="text-xl font-semibold mb-4">Manager Dashboard</h2>
           <p>This is where you can manage movies, reservations, users, and more.</p>
-
 
           <div className="mt-6">
             <h3 className="text-lg font-medium">Overview</h3>
             <p>Here you can see an overview of recent activity, analytics, and stats.</p>
           </div>
+
         </div>
       </div>
     </div>
