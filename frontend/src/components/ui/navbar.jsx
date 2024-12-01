d1801918cafe0d6c5208c7efa08ade1999364499
mohammed-rharbi <rharbi383@gmail.com>
2024-12-01 12:22:59 +0100

    chore: refine navbar for improved navigation

--- frontend/src/components/ui/navbar.jsx
+++ frontend/src/components/ui/navbar.jsx
@@ -1,6 +1,7 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../hooks/authContext'; 
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 export default function Navbar() {
@@ -12,7 +13,7 @@ export default function Navbar() {
     setMobileMenuOpen(!mobileMenuOpen);
   };
 
-
+  
   const navigate = useNavigate()
 
   const handleLogout = () => {
@@ -20,6 +21,11 @@ export default function Navbar() {
 	navigate('/login')
 
   };
+
+  if(!user){
+
+    return navigate('/login')
+  }
 
   return (
     <nav className="bg-gray-800">
@@ -58,26 +64,23 @@ export default function Navbar() {
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex shrink-0 items-center">
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
-                alt="Your Company"
-              />
+             <h1 className='text-2xl font-bold text-white'>R.S</h1>
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
-                <a href="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">
-                  Dashboard
-                </a>
-                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
-                  Team
-                </a>
-                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
-                  Projects
-                </a>
-                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
-                  Calendar
-                </a>
+
+                <Link to={'/home'}className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="home">
+                Home
+                </Link>
+       
+                <Link to={'/myEvents'}className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="home">
+                My Events
+                </Link>
+
+                <Link to={'/home'}className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="home">
+                Home Page
+                </Link>
+
               </div>
             </div>
           </div>
